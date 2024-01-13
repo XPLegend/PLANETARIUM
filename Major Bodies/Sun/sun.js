ffe4b53b3484247a6ac392bcf2e3c61b24fce3f1
XPLegend <marcocarnemolla07@gmail.com>
2024-01-13 19:53:56 +0100

    Working on the sun

---
+++ Major Bodies/Sun/sun.js
@@ -0,0 +1,11 @@
+//change image image on hover
+function chgImgBackBtn() {
+
+    document.getElementById("backBtnImg").src = "../../Other/General Assets/Back-Icon-Hover.png";
+}
+
+//change image back
+function chgImgBackBtnBACK() {
+
+    document.getElementById("backBtnImg").src = "../../Other/General Assets/Back-Icon.png";
+}
